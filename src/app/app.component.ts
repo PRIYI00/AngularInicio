c994e984e89194a96d5ab9a285c2c99850a56eab
ikerpedrejon@gmail.com <ikerpedrejon@gmail.com>
2020-01-17 09:42:39 +0100

    App Angular Inicio

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
 
+// Componente Principal que se carga al Arrancar la App.
+// Selector: Nombre de la Etiqueta para inyectar componente en HTLML.
+// TemplateUrl: Vista del HTML.
+// StyleUrls: Estilos para la Vista formato scss.
+
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
+  selector: 'app-root', 
+  templateUrl: './app.component.html', 
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+  // Variable
   title = 'AngularApp';
 }
